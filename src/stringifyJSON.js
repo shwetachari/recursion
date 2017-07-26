20927f9d4799ffc3ec1ea6cb892bfdfcdb0934a3
Shweta Patrachari <shweta@Shwetas-MacBook-Pro.local>
2017-07-26 15:29:40 -0700

    modified stringifyJSON function to pass all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // BASE CASE: primitives, 2ndary: arrays, 3rd: objs
+  if(typeof(obj) === 'function' || obj === undefined || obj === null) {
+    return 'null';
+  } else if(typeof(obj) === 'number' || typeof(obj) === 'boolean') {
+    return '' + obj;
+  } else if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }else if (Array.isArray(obj)) {
+    if(obj.length === 0) {
+      return '[]';
+    } else {
+      var arrayVals = obj.map(function(objItem){
+        return stringifyJSON(objItem);
+      });
+      return '[' + arrayVals.toString() + ']';
+    }
+  } else if (typeof(obj) === 'object') {
+    var objArray = (Object.keys(obj)).map(function(key) {
+      if(typeof(obj[key]) === 'function' || obj[key] === undefined) {
+        return '';
+      }
+      return '"' + key + '":' + stringifyJSON(obj[key]);
+    });
+    var allNull = objArray.every(function(obj) {
+      return obj === '';
+    });
+    if (allNull) {
+      return '{' + objArray.join('') + '}'
+    }
+    return '{' + objArray.join(',') + '}';
+  }
 };
